64dbe4bd84bcc7cb8b66398fdec3af82c23aa17f
Kristian Kjerstad <kristian.kjerstad@brilliant.no>
2024-06-14 15:15:15 +0200

    add graceful shufdown and error handling

--- application/app.go
+++ application/app.go
@@ -4,27 +4,61 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/redis/go-redis/v9"
 )
 
 type App struct {
-	router http.Handler
+	router  http.Handler
+	redisDB *redis.Client
 }
 
 func New() *App {
-	app := &App{router: loadRoutes()}
+	app := &App{
+		router:  loadRoutes(),
+		redisDB: redis.NewClient(&redis.Options{}),
+	}
 
 	return app
 }
 
-func (app *App) Start(context context.Context) error {
+func (app *App) Start(ctx context.Context) error {
 	server := &http.Server{
 		Addr:    ":3000",
 		Handler: app.router,
 	}
 
-	err := server.ListenAndServe()
+	err := app.redisDB.Ping(ctx).Err()
 	if err != nil {
-		return fmt.Errorf("Failed to start server %w", err)
+		return fmt.Errorf("Failed to connect to redis: %w", err)
 	}
+
+	defer func() {
+		if err := app.redisDB.Close(); err != nil {
+			fmt.Println("Failed to close redis", err)
+		}
+	}()
+
+	fmt.Println("Starting server")
+	ch := make(chan error, 1)
+
+	go func() {
+		err = server.ListenAndServe()
+		if err != nil {
+			ch <- fmt.Errorf("Failed to start server %w", err)
+		}
+		close(ch)
+	}()
+
+	select {
+	case err = <-ch:
+		return err
+	case <-ctx.Done():
+		timeout, cancel := context.WithTimeout(context.Background(), time.Second*10)
+		defer cancel()
+		return server.Shutdown(timeout)
+	}
+
 	return nil
 }
--- main.go
+++ main.go
@@ -3,14 +3,23 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+	"os/signal"
 	"project/application"
 )
 
 func main() {
 	app := application.New()
 
-	error := app.Start(context.TODO())
+	//allow graceful shutdown upon signal to terminate (ctrl + C) - we want to finish database operations and so on BEFORE shut down
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	error := app.Start(ctx)
 	if error != nil {
 		fmt.Println("Failed to start app", error)
 	}
+
 }
+
+// https://www.youtube.com/watch?v=qCv-q37qjZU
